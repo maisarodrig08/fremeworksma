52764a37550036b4e83e691353c4aca60f3bffe1
maisarodrig08 <maisa.souza@estudante.ifms.edu.br>
2026-05-15 11:21:32 -0400

    aula

--- src/auth/repository/find-user-by-email.repository.ts
+++ src/auth/repository/find-user-by-email.repository.ts
@@ -3,9 +3,10 @@ import { PrismaService } from 'src/shared/databases/prisma.database';
 
 @Injectable()
 export class FindUserByEmailRepository {
+
   constructor(private readonly prisma: PrismaService) {}
 
-  async findUserByEmail(email: string) {
+  async findByEmail(email: string) {
     return await this.prisma.user.findUnique({
       where: { email },
     });
